449e99abeb35d97756f4a21532ba204d74eef5f7
Sudin Joshi <sudinjoshi13@gmail.com>
2024-09-27 23:16:54 -0500

    Added Login page and Register Page

--- campusxchange-ui/src/App.tsx
+++ campusxchange-ui/src/App.tsx
@@ -1,14 +1,73 @@
-import React from 'react'
-import './App.css'
+import { Component } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>campusXchange</p>
-            </header>
-        </div>
-    )
+import IUser from './type/user';
+
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+type Props = {};
+
+type State = {
+  currentUser: IUser | undefined
 }
 
-export default App
+class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      currentUser: undefined,
+    };
+  }
+
+  render() {
+    const { currentUser } = this.state;
+
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          {currentUser ? (
+            <div className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link to={"/profile"} className="nav-link">
+                  {currentUser.username}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link">
+                  LogOut
+                </a>
+              </li>
+            </div>
+          ) : (
+            <div className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to={"/register"} className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </div>
+          )}
+        </nav>
+
+        <div className="container mt-3">
+          <Routes>
+            <Route path="/login" element={<Login/>} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
