081882e32e712c9eddb8815f01f6d038351039b3
amrit <bluestone@Amrits-MacBook-Pro-2.local>
2024-09-30 17:19:05 -0500

    Login API bean validation, expception handling, Role-based access control configuration

--- campusxchange/src/main/java/com/unt/campusxchange/users/config/SecurityConfiguration.java
+++ campusxchange/src/main/java/com/unt/campusxchange/users/config/SecurityConfiguration.java
@@ -12,10 +12,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -26,9 +29,12 @@ import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthen
 import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.stream.Collectors;
+
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration {
 
     private final JWTConfigProperties jwtConfigProperties;
@@ -76,4 +82,11 @@ public class SecurityConfiguration {
             throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
+
+//    @Bean
+//    public GrantedAuthoritiesMapper authoritiesMapper() {
+//        return authorities -> authorities.stream()
+//                .map(authority -> new SimpleGrantedAuthority(authority.getAuthority()))
+//                .collect(Collectors.toList());
+//    }
 }
