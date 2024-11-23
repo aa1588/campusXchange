268cd1f3c9f3f91d0979ccc8e51b5b9fcaace8b9
amrit <bluestone@Amrit-2.local>
2024-11-22 20:04:55 -0600

    fix spotless error

--- campusxchange/src/test/java/com/unt/campusxchange/recommendations/service/RecommendationServiceTest.java
+++ campusxchange/src/test/java/com/unt/campusxchange/recommendations/service/RecommendationServiceTest.java
@@ -1,4 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.unt.campusxchange.recommendations.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 class RecommendationServiceTest {
-  
+
+    @Test
+    void shouldReturnRecommendations() {
+
+    }
 }
