58ba9c3b1a8a2f0ea25360e4747a91f884cf3564
Sudin Joshi <sudinjoshi13@gmail.com>
2024-11-24 12:57:59 -0600

    Accept offer

--- campusxchange-ui/src/modules/items/service/OfferService.ts
+++ campusxchange-ui/src/modules/items/service/OfferService.ts
@@ -3,13 +3,13 @@ import Cookies from 'js-cookie'
 import {BACKEND_BASE_URL} from "../../../config/config";
 
 const OFFER_CREATE_API_URL = `${BACKEND_BASE_URL}/api/offers/create`
+const OFFER_ALL_MY_ITEMS_API_URL = `${BACKEND_BASE_URL}/api/offers/allitemsoffer`
+const API_OFFER = `${BACKEND_BASE_URL}/api/offers`
 
 class OfferService {
 
     makeAnOffer = async (itemId : number, amount : number) => {
         const token = Cookies.get('authToken');
-
-        debugger;
 
         try{
             await axios.post(`${OFFER_CREATE_API_URL}/${itemId}`, {"amount" : amount}, {
@@ -28,6 +28,50 @@ class OfferService {
             }
         }
 
+    }
+
+    getAllMyListingsOffer = async () => {
+        const token = Cookies.get('authToken');
+
+        try{
+            const response = await axios.get(`${OFFER_ALL_MY_ITEMS_API_URL}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                },
+            })
+            return response.data;
+        }catch (error) {
+            if (axios.isAxiosError(error)) {
+                throw new Error(
+                    error.response?.data?.message || 'Error getting offers'
+                )
+            } else {
+                throw new Error('An unknown error occurred')
+            }
+        }
+    }
+
+    acceptOffer = async (offerid : number) => {
+        const token = Cookies.get('authToken');
+
+        try{
+            const response = await axios.put(`${API_OFFER}/${offerid}/accept`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                },
+            })
+            return response.data;
+        }catch (error) {
+            if (axios.isAxiosError(error)) {
+                throw new Error(
+                    error.response?.data?.message || 'Error accepting offers'
+                )
+            } else {
+                throw new Error('An unknown error occurred')
+            }
+        }
     }
 }
 
